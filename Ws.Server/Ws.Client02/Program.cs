e07f3c61bbab7fe26099805e9c9a0d32f331a649
Joao Vitor Almeida Pimentel <agaphoni@hotmail.com>
2021-11-25 14:43:35 -0300

    webSocket chat

---
+++ Ws.Server/Ws.Client02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ws.Client02
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("aperte enter para começar");
+            Console.ReadLine();
+
+            using (ClientWebSocket client = new ClientWebSocket())
+            {
+                Uri serviceUri = new Uri("ws://localhost:5000/send");
+                var cTs = new CancellationTokenSource();
+                cTs.CancelAfter(TimeSpan.FromSeconds(120));
+                try
+                {
+                    await client.ConnectAsync(serviceUri, cTs.Token);
+                    var n = 0;
+                    while (client.State == WebSocketState.Open)
+                    {
+                        Console.WriteLine("escreva uma menssagem para enviar");
+                        string message = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(message))
+                        {
+                            ArraySegment<byte> byteToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+                            await client.SendAsync(byteToSend, WebSocketMessageType.Text, true, cTs.Token);
+                            var responseBuffer = new byte[1024];
+                            var offset = 0;
+                            var packet = 1024;
+                            while (true)
+                            {
+                                ArraySegment<byte> byteRecieved = new ArraySegment<byte>(responseBuffer, offset, packet);
+                                WebSocketReceiveResult response = await client.ReceiveAsync(byteRecieved, cTs.Token);
+                                var responseMessage = Encoding.UTF8.GetString(responseBuffer, offset, response.Count);
+                                Console.WriteLine(responseMessage);
+                                if (response.EndOfMessage)
+                                    break;
+                            }
+                        }
+                    }
+                }
+
+                catch (WebSocketException e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
